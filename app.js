57f3a6ab7cda79fd43a0126ec3f126d2eaee8db5
daudi13 <davouma13@gmail.com>
2021-08-18 16:42:13 +0300

    commit sec

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //add active class on click
-const dot = document.querySelector('.dot');
+const sec = document.querySelector('.dot');
+const dot = document.querySelector('.sec');
 dot.onclick = function () {
-    dot.classList.toggle('active')
+    dot.classList.toggle('active');  
+    sec.classList.toggle('active');
 }
 
 
@@ -9,4 +11,4 @@ document.addEventListener('mousemove', function (e) {
     const dot = document.querySelector('.dot');
     dot.style.left = e.pageX + 'px';
     dot.style.top = e.pageY + 'px';
-})
+});
