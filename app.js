4137a3f9d42652dc80f1989c11d5cec4b9bb3921
daudi13 <davouma13@gmail.com>
2021-08-18 16:29:44 +0300

    commit mousemove

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-document.addEventListener
+document.addEventListener('mousemove', function (e) {
+    const dot = document.querySelector('.dot');
+    dot.style.left = e.pageX + 'px';
+    dot.style.top = e.pageY + 'px';
+})
